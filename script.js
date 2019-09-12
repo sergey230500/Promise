90ebc705469439e1a1644277013bbf2402fe008e
sergey230500 <sergey230500@mail.ru>
2019-09-12 22:14:19 +0300

    r

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ function sieve(n){
 	}
 	return primes;
 }
+
+function PrimeFinder(n){
+	this.n = n;
+	this.map = {1: true};
+	this.primes = [];
+	this.position = 2;
+	
+}
+PrimeFinder.prototype = {
+	continue: function(iterations){
+		for (; this.position <= this.n && iterations > 0; this.position++, iterations--){
+			if (this.position in this.map){
+				delete this.map[this.position];
+			}else{
+				this.primes.push(this.position);
+				for (let j = this.position; j <= this.n; j += this.position) this.map[j] = true;
+			}
+		}
+		if (this.position >= this.n) {
+			this.map = null;
+			return true;
+		}
+		return false;
+	},
+	sieve: function(){
+		let self = this;
+		return new Promise(function(resolve, reject) {
+		  setTimeout(function step() {
+			if (self.continue(5000)) resolve(self.primes);
+			else setTimeout(step, 20);
+		  }, 0);
+		});
+	}
+}
