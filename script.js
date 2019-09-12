e157f8db6041f08da46ab8869b31d0785e4045a1
blr-Oliver <vasily.liaskovsky@gmail.com>
2019-09-12 23:09:21 +0300

    кусочная обработка длинных интервалов

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
-function sieve(n){
-	let map = {1: true};
-	let primes = [];
-	for (let i = 2; i <= n; i++){
-		if (i in map){
-			delete map[i];
-		}
-		else{
-			primes.push(i);
-			for (let j = i; j <= n; j += i) map[j] = true;
-		}
-	}
-	return primes;
+function sieve(n) {
+  let map = {1: true};
+  let primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (i in map) {
+      delete map[i];
+    } else {
+      primes.push(i);
+      for (let j = i; j <= n; j += i) map[j] = true;
+    }
+  }
+  return primes;
 }
 
-function PrimeFinder(n){
-	this.n = n;
-	this.map = {1: true};
-	this.primes = [];
-	this.position = 2;
-	
+function PrimeFinder(n) {
+  this.n = n;
+  this.map = {};
+  this.primes = [2];
+  this.position = 3;
 }
+
 PrimeFinder.prototype = {
-	continue: function(iterations){
-		for (; this.position <= this.n && iterations > 0; this.position++, iterations--){
-			if (this.position in this.map){
-				delete this.map[this.position];
-			}else{
-				this.primes.push(this.position);
-				for (let j = this.position; j <= this.n; j += this.position) this.map[j] = true;
-			}
-		}
-		if (this.position >= this.n) {
-			this.map = null;
-			return true;
-		}
-		return false;
-	},
-	sieve: function(){
-		let self = this;
-		return new Promise(function(resolve, reject) {
-		  setTimeout(function step() {
-			if (self.continue(5000)) resolve(self.primes);
-			else setTimeout(step, 20);
-		  }, 0);
-		});
-	}
-}
+  continue: function (iterations) {
+    const stepLimit = Math.min(this.position + iterations, this.n);
+
+    for (let lastKnownPrime = this.primes[this.primes.length - 1];
+         lastKnownPrime * lastKnownPrime < stepLimit;
+         lastKnownPrime = this.primes[this.primes.length - 1]) {
+      let preIterations = lastKnownPrime * lastKnownPrime - this.position;
+      this.continue(preIterations);
+    }
+
+    const lastPrime = Math.floor(Math.sqrt(stepLimit));
+
+    for (let i = 0, p; (p = this.primes[i]) <= lastPrime; ++i)
+      for (let j = Math.ceil(this.position / p) * p; j <= stepLimit; j += p) this.map[j] = true;
+
+    for (; this.position <= stepLimit; this.position++) {
+      if (this.position in this.map)
+        delete this.map[this.position];
+      else
+        this.primes.push(this.position);
+    }
+
+    if (this.position >= this.n) {
+      this.map = null;
+      return true;
+    }
+
+    return false;
+  },
+  sieve: function () {
+    let self = this;
+    return new Promise(function (resolve, reject) {
+      setTimeout(function step() {
+        if (self.continue(5000)) resolve(self.primes);
+        else setTimeout(step, 1);
+      }, 0);
+    });
+  }
+};
+//5761455
